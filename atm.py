b2019d68edc026765b89bc90df92329b76fa8c9f
Shivang <shivangkainth94@gmail.com>
2026-06-06 11:19:09 +0530

    basic skeleton completed now database connection next

--- atm.py
+++ atm.py
@@ -1,40 +1,37 @@
 print("-----------------Welcome To ATM-----------------")
-
-# the progress has made
-# now the remaining logic is to set three tries at the pin check upon wrong entry 
-# and the pin verify logic if the pin is even set already or still none
-
-
 class ATM:
     def __init__(self,):
         self.balance = 0
         self.pin = None
         self.card_no = 0
+        self.limit = 0
         self.menu()
 
     def menu(self):
         while True:
-            print("""How would you like to proceed ?  
-                           1. Enter 1 to create pin.
-                           2. Enter 2 to deposit.
-                           3. Enter 3 to withdraw
-                           4. Enter 4 to check balance.
-                           5. Enter 5 to exit.""")
-            num = int(input("Enter your input: "))
-            if num == 1:
-                self.create_pin()
-            elif num == 2:
-                self.deposit()
-            elif num == 3:
-                self.withdraw()
-            elif num == 4:
-                self.check_balance()
-            elif num == 5:
-                print("See you later")
-                break
+            if self.valid_tries() == True:
+                print("""How would you like to proceed ?  
+                            1. Enter 1 to create pin.
+                            2. Enter 2 to deposit.
+                            3. Enter 3 to withdraw
+                            4. Enter 4 to check balance.
+                            5. Enter 5 to exit.""")
+                num = int(input("Enter your input: "))
+                if num == 1:
+                    self.create_pin()
+                elif num == 2:
+                    self.deposit()
+                elif num == 3:
+                    self.withdraw()
+                elif num == 4:
+                    self.check_balance()
+                elif num == 5:
+                    print("See you later")
+                    break
+                else:
+                    print("Enter a valid choice")
             else:
-                print("Enter a valid choice")
-        
+                break
 
     def create_pin(self):
        pin = int(input("Enter your pin: "))
@@ -42,49 +39,52 @@ class ATM:
        print("pin created successfully")
 
     def deposit(self):
-        if self.pin != None:
-            temp = int(input("Enter you pin: "))
-            if temp == self.pin:
-                amt = int(input("Enter the amount want to deposit: "))
-                self.balance += amt
-                print("Amount successfuly deposited")
-            else:
-                print("Envalid pin !")
+        if self.pin_validation() == True:
+            amt = int(input("Enter the amount want to deposit: "))
+            self.balance += amt
+            print("Amount successfuly deposited")
         else:
-            print("Set your pin first")
+            pass
 
     def withdraw(self):
-        if self.pin != None:
-            temp = int(input("Enter your pin: "))
-            if temp == self.pin:
-                amt = int(input("Enter the amount want to withdraw: "))
-                if amt <= self.balance:
-                    print("Amount withdraw successfully")
-                    self.balance -= amt
-                else:
-                    print("Wrong amount or Low balance")
+        if self.pin_validation() == True:
+            amt = int(input("Enter the amount want to withdraw: "))
+            if amt <= self.balance:
+                print("Amount withdraw successfully")
+                self.balance -= amt
             else:
-                print("Envalid pin")
+                print("Wrong amount or Low balance")
         else:
-            print("Set your pin first")
+            pass
 
     def check_balance(self):
-        if self.pin != None:
-            temp = int(input("Enter your pin: "))
-            if self.pin == temp:
-                print(f"Balance: {self.balance}")
-            else:
-                print("Invalid pin")
+        if self.pin_validation() == True:
+            print(f"Balance: {self.balance}")
         else:
-            print("Set your pin first")
+            pass
+
+    def valid_tries(self):
+        if self.limit < 3:
+            return True
+        else:
+            print("Your maximum limit of invalid tries have reached. Try again after two days or confirmation")
+            return False
+        
+    def pin_validation(self):
+        if self.pin == None:
+            print("Choose 1 to set your pin first then move to next option")
+            return False
+        temp = int(input("Enter your pin: "))
+        if temp == self.pin:
+            return True
+        else:
+            self.limit += 1
+            print(f"wrong pin entered. wrong tries {self.limit} total wrong tries are 3")
+            return False
+
 
           
 
     
 
 x = ATM()
-# x = input("Enter your balance")
-# y = input("Enter your Pin")
-
-# z = ATM(x,y)
-# print(z.balance,z.pin)
